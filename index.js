847cbf203a6a12e36486e7bc008d134719e705aa
Alyssa Cuson <acuson98@gmail.com>
2022-01-26 14:40:31 -0500

    connected to database

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // Require dependencies
 const consoleTable = require('console.table');
 const inquirer = require('inquirer');
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
 
+// Connect to MySQL database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ju7ft6a;p0',
+    database: 'employee_db'
+},
+console.log('Connected to employee_db database.')
+);
 
+// Inquirer questions
+
+// View all departments
+
+// View all roles
+
+// View all employees
+
+// Add department
+
+// Add role
+
+// Add employee
+
+// Update employee role
 
