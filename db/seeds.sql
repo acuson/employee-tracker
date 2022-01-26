c771b657ada26cca61a8b88dfd5039f156827698
Alyssa Cuson <acuson98@gmail.com>
2022-01-26 12:04:10 -0500

    refactored for properly inserted values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,30 @@
-INSERT INTO department (id, name)
-VALUES (1,"Sales"),
-       (2,"Engineering"),
-       (3,"Finance"),
-       (4,"Legal");
+INSERT INTO department (name)
+VALUES 
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (11,"Sales Representative",50000,1),
-       (12,"Sales Manager",90000,1),
-       (21,"Junior Developer",70000,2),
-       (22,"Senior Software Engineer",100000,2),
-       (23,"Head of Engineering",200000,2),
-       (31,"Financial Analyst",55000,3),
-       (32,"Senior Financial Anlyst",80000,3),
-       (33,"Director of Finance",120000,3),
-       (41,"Corporate Attorney",250000,4);
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ("Sales Representative",50000,1),
+    ("Sales Manager",90000,1),
+    ("Junior Developer",70000,2),
+    ("Senior Software Engineer",100000,2),
+    ("Head of Engineering",200000,2),
+    ("Financial Analyst",55000,3),
+    ("Senior Financial Anlyst",80000,3),
+    ("Director of Finance",120000,3),
+    ("Corporate Attorney",250000,4);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)  
-VALUES (001,"Edward","Butler",11,12),
-       (002,"Julia","Barnes",12),
-       (003,"Tammy","Martin",21,22),
-       (004,"Steven","Perry",22,23),
-       (005,"Patrick","Richardson",23),
-       (006,"Amy","Peterson",31,32),
-       (007,"Laura","Lopez",32,33),
-       (008,"Earl","Ward",33),
-       (009,"Amanda","Parker",41);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)  
+VALUES 
+    ("Edward","Butler",1,2),
+    ("Julia","Barnes",2,NULL),
+    ("Tammy","Martin",3,5),
+    ("Steven","Perry",4,5),
+    ("Patrick","Richardson",5,NULL),
+    ("Amy","Peterson",6,8),
+    ("Laura","Lopez",7,8),
+    ("Earl","Ward",8,NULL),
+    ("Amanda","Parker",9,NULL);
